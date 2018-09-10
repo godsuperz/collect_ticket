7486e0a7d1171675490b3e47054d56403fe8f1a7
SuperXZ <lovezhangchao@outlook.com>
2018-09-10 11:13:43 +0800

    基本环境搭建成功，可访问localhost:8080/Test/hello进行测试

---
+++ src/main/java/com/super223/CollectTicketApplication.java
@@ -0,0 +1,12 @@
+package com.super223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectTicketApplication.class, args);
+    }
+}
